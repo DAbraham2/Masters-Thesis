e864607bbb14f8e907c5d8b9c344b251c7329b9d
DAbraham2 <daniel.abraham@edu.bme.hu>
2023-11-13 11:34:46 +0100

    config stuff

--- src/gecko_spec_test/main.py
+++ src/gecko_spec_test/main.py
@@ -1,6 +1,9 @@
-def __main() -> None:
-    print('henlo')
 
+very_long_Filename='abnsdégfubaéwerguiasdnvgipauwefbipauszg1i24gbriezbfpaisdfihké32bvf876xcgyYVB341o7r46fgcvSLDUZF'
+
+def __main() -> None:
+
+    print('henlo')
 
 if __name__ == '__main__':
     __main()
